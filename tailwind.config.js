583bb4aae0114cb73ccd1e0a377db29dabbca317
Thai Truong <sontruong@hotmail.co.uk>
2021-10-25 00:30:44 +0100

    fist commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: "Roboto",
+      },
+      textColor: {
+        primary: "#AC1D03",
+        secondary: "#333333",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
