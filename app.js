8bb1b7e8cfafce6b27a88333e1c1f9278aef1cd2
Thai Truong <sontruong@hotmail.co.uk>
2021-10-28 00:24:04 +0100

    accordion

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const burger = document.querySelector(".burger");
 const cross = document.querySelector(".cross");
 const mobileMenu = document.querySelector(".mobileMenu");
+const plus = document.getElementById("plus");
+const content = document.querySelector(".content");
+const accordion = document.querySelector(".accordion");
 
 //toggle nav
 
 burger.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
+});
+
+accordion.addEventListener("click", () => {
+  content.classList.toggle("hidden");
+});
+
+plus.addEventListener("click", () => {
+  content.classList.toggle("hidden");
 });
